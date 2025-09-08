53e78b4367f124936fa7fcb265ccd538cafa031e
Esa Jokinen <esa@esajokinen.net>
2025-09-08 08:07:37 +0300

    chg: [import] refactor legacy dictionary imports
    Refactored the legacy XML CPE dictionary import logic into dedicated
    handler classes, introducing an abstract base class and an XML-specific
    implementation. Also added a downloader class, preparing the codebase
    for cleaner separation of responsibilities and future format support.

---
+++ lib/cpeimport/__init__.py
@@ -0,0 +1,9 @@
+from .base import CPEImportHandler
+from .xml_dictionary import XMLCPEHandler
+from .downloader import CPEDownloader
+
+__all__ = [
+    "CPEImportHandler",
+    "XMLCPEHandler",
+    "CPEDownloader",
+]
